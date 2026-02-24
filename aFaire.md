704a0472d470608e2cd9f61e2b9faeae75a12368
Lorelei13120 <hypericumperforatum13120@protonmail.com>
2026-02-24 23:13:38 +0100

    fichier a faire.md ajouté

---
+++ aFaire.md
@@ -0,0 +1,10 @@
+TRUCS A FAIRE 24/04/25
+1) Si les messages dans "Actualités" ne contienne pas de texte, mais simplement une image, alors enlever cette entrée et simplement laisser l'image dans la Galerie photo.
+2) Est-ce qu'il y a bien un id du dernier message sauvegardé quelpart, pour que le bot sache bien à partir de quel message fetch lors du lancement du script avec Github Actions ?
+3) Les images ne s'affichent pas correctement dans les sections "actualités" et "événements"
+4) Dans la gallerie, affiche les photos les plus récentes en premier dans la partie "photos" et les affiches les plus récentes en premier dans la partie "Affiches"
+5) Il y a dans "Assets" les 4 images suivantes : "dance-day.jpg", "pontareuse-devant.jpg", "planter-hirondelles.jpg", "greffe.jpg".
+J'aimerais les supprimer et donc enlever dans tout le projet les appels qui y sont fait. En effet, je n'ai plus besoin de ces images
+car toutes les images sont enregistrées dans public/Assets désormais.
+6) Mets bien à jour les pages "Evenements" et "Actualités"
+7) Bonus : faire une traduction de tout le site en allemand hehe, mais bon c'est du bonus hein, on verra plus tard lol
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,16 +4,6 @@ import NewsSection from "@/components/NewsSection";
 import EventsSection from "@/components/EventsSection";
 import FooterSection from "@/components/FooterSection";
 
-//PROMPTS ET TRUCS A FAIRE 24/04/25
-// //1) Si les messages dans "Actualités" ne contienne pas de texte, mais simplement une image, alors enlever cette entrée et simplement laisser l'image dans la Galerie photo.
-//3) Est-ce qu'il y a bien un id du dernier message sauvegardé quelpart, pour que le bot sache bien à partir de quel message fetch lors du lancement du script avec Github Actions ?
-//2) Les images ne s'affichent pas correctement dans les sections "actualités" et "événements"
-//3) Dans la gallerie, affiche les photos les plus récentes en premier dans la partie "photos" et les affiches les plus récentes en premier dans la partie "Affiches"
-//4) Il y a dans "Assets" les 4 images suivantes : "dance-day.jpg", "pontareuse-devant.jpg", "planter-hirondelles.jpg", "greffe.jpg".
-//J'aimerais les supprimer et donc enlever dans tout le projet les appels qui y sont fait. En effet, je n'ai plus besoin de ces images
-//car toutes les images sont enregistrées dans public/Assets désormais.
-//5) Mets bien à jour les pages "Evenements" et "Actualités"
-
 const Index = () => {
   return (
     <main>
