84e86629bbca320d38bb1dd3a166083c8a9b2584
LelioB <120948059+Lelio88@users.noreply.github.com>
2026-05-06 08:48:33 +0200

    fix: supprime imports morts dans Index.tsx et reactive no-unused-vars
    NewsSection et EventsSection etaient importes dans Index.tsx mais
    jamais utilises dans le JSX. La regle ESLint no-unused-vars etait
    desactivee ('off'), ce qui masquait exactement ce genre d'imports
    morts.
    
    Reactive a 'warn' avec le pattern d'echappement '^_' (convention
    TypeScript pour les vars volontairement inutilisees).
    
    Closes #13

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,5 @@
 import HeroSection from "@/components/HeroSection";
 import DescriptionSection from "@/components/DescriptionSection";
-import NewsSection from "@/components/NewsSection";
-import EventsSection from "@/components/EventsSection";
 import FooterSection from "@/components/FooterSection";
 
 const Index = () => {
